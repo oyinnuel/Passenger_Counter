c24b20965896c25e41d00580337ffd0067797c01
oyinnuel <47600584+oyinnuel@users.noreply.github.com>
2022-06-30 11:51:16 +0100

    Initializing files for Passenger counter

---
+++ index.js
@@ -0,0 +1,19 @@
+let count = 0
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
+
+console.log("Let's count people on the subway!")
+
+// How do I make the save button to be disabled when the count is zero(0)?
